47ed1d62f16e4e261a2327e471bf176087d7a4aa
EthanLiHPQ <141368089+EthanLiHPQ@users.noreply.github.com>
2023-08-22 20:41:39 +0800

    Add files via upload

---
+++ question.js
@@ -0,0 +1,62 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Question 1",
+        answer: "Answer 1",
+        options:[
+            "Answer 1",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4",
+        ]
+
+    },
+    {
+        numb: 2,
+        question: "Question 2",
+        answer: "Answer 1",
+        options:[
+            "Answer 1",
+            "Answer 2e",
+            "Answer 3",
+            "Answer 4",
+        ]
+
+    },
+    {
+        numb: 3,
+        question: "Question 3",
+        answer: "Answer 1",
+        options:[
+            "Answer 1",
+            "Answer 2",
+            "Answer 3d",
+            "Answer 4",
+        ]
+
+    },
+    {
+        numb: 4,
+        question: "Question 4",
+        answer: "Answer 1",
+        options:[
+            "Answer 1q",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4",
+        ]
+
+    },
+    {
+        numb: 5,
+        question: "Question 5",
+        answer: "Answer 1",
+        options:[
+            "Answer 1",
+            "Answer 2",
+            "Answer 3a",
+            "Answer 4",
+        ]
+
+    }
+]
